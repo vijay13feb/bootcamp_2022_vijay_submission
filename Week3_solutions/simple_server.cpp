47bd7488470add5dd28e2835357940eaaa776463
vijay13feb <vijaysi@iiitd.ac.in>
2022-06-22 17:02:00 +0530

    Week3_solutions

--- Week3_solutions/simple_server.cpp
+++ Week3_solutions/simple_server.cpp
@@ -1,75 +1,126 @@
-/* run using ./server <port> */
+// socket server example, handles multiple clients using threads
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <netinet/in.h>
-
-#include <pthread.h>
-
-void error(char *msg) {
-  perror(msg);
-  exit(1);
-}
-
-int main(int argc, char *argv[]) {
-  int sockfd, newsockfd, portno;
-  socklen_t clilen;
-  char buffer[256];
-  struct sockaddr_in serv_addr, cli_addr;
-  int n;
-
-  if (argc < 2) {
-    fprintf(stderr, "ERROR, no port provided\n");
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<stdlib.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<pthread.h> //for threading , link with lpthread
+#include"http_server.hh"
+//the thread function
+void *start_function(void *);
+HTTP_Response *response;
+char buffer[2000];
+void error(char *msg)
+{
+    perror(msg);
     exit(1);
-  }
+}
+int main(int argc , char *argv[])
+{
+    int sockfd, newsockfd , c , *new_sock;
+    int  portno;
+    
+    struct sockaddr_in server , client;
 
-  /* create socket */
+    //Create socket
+    sockfd = socket(AF_INET , SOCK_STREAM , 0);
+    if (sockfd == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0)
-    error("ERROR opening socket");
+    //Prepare the sockaddr_in structure
+    portno = atoi(argv[1]);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(portno);
 
-  /* fill in port number to listen on. IP address can be anything (INADDR_ANY)
-   */
+    //Bind
+    if( bind(sockfd,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
 
-  bzero((char *)&serv_addr, sizeof(serv_addr));
-  portno = atoi(argv[1]);
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
+    //Listen
+    listen(sockfd , 3);
 
-  /* bind socket to this port number on this machine */
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
 
-  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    error("ERROR on binding");
+        c=sizeof(struct sockaddr_in);
+       while(newsockfd=accept(sockfd,(struct sockaddr*)&client,(socklen_t*)&c))
+       {
+        puts("Connection accepted");
 
-  /* listen for incoming connection requests */
+        pthread_t sniffer_thread;
+        new_sock = (int*)malloc(1);
+        *new_sock = newsockfd;
 
-  listen(sockfd, 5);
-  clilen = sizeof(cli_addr);
+        if( pthread_create( &sniffer_thread , NULL ,  start_function , (void*) new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
 
-  /* accept a new request, create a newsockfd */
+        puts("Working thread assigned");
+    }
 
-  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-  if (newsockfd < 0)
-    error("ERROR on accept");
+    if (newsockfd < 0)
+    {
+        error("accept failed");
+        return 1;
+    }
+    return 0;
+}
+/*
+  This will handle connection for each client
+  */
+void *start_function(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int n, temp;
 
-  /* read message from client */
+        //char    sendBuff[100], client_message[2000];
+        bzero(buffer, 2000);
 
-  bzero(buffer, 256);
-  n = read(newsockfd, buffer, 255);
-  if (n < 0)
-    error("ERROR reading from socket");
-  printf("Here is the message: %s", buffer);
+      while((n=read(sock,buffer, 2000))>0)
+      {
+         
+        //cout <<out<< endl;
+        //char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world123";
+        //printf("Here is the message: %s", buffer);
+        response = handle_request(buffer);
+        //cout << "Request is handled"<<endl; 
+        cout <<buffer << endl;
+        
+        string out = response->get_string();
+        strcpy(buffer, out.c_str());
 
-  /* send reply to client */
-
-  n = write(newsockfd, "I got your message", 18);
-  if (n < 0)
-    error("ERROR writing to socket");
-
-  return 0;
+        
+        //cout <<strlen(buffer)<< endl;
+        temp = write(sock, buffer, strlen(buffer));
+        
+        bzero(buffer, 2000);
+        n = read(sock, buffer, 2000);
+        
+      }
+      
+      close(sock);
+      if(n==0)
+      {
+        puts("Client Disconnected");
+      }
+      else
+      {
+        error("recv failed");
+      }
+    return 0;
 }
